3d617b2f0816f2d841d123964c71f374efea8e88
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-14 23:34:28 +0530

    Assignment 2 completed

---
+++ Assignment 4/Question-2.js
@@ -0,0 +1,13 @@
+// Q.2 Explain Temporal Dead Zone?
+
+// Ans: Temporal Dead Zone is a behavior that occurs when trying to access
+// a variable that has been declared but not yet initialized. This behavior can cause unexpected errors in code.
+// During temporal dead zone which is the period between the start of the scope and the actual declaration of the variable, accessing the variable would result in a ReferenceError. This is because the variable exists in the scope but has not been assigned a value yet.
+
+console.log(myVariable); // ReferenceError: Cannot access 'myVariable' before initialization
+
+let myVariable = 10;
+console.log(myVariable);
+
+// Above, the first console.log statement throws a ReferenceError because myVariable is accessed before it is declared. This is because the variable is in the temporal dead zone until it reaches the line where it is declared.
+// Once the variable declaration is encountered, it is assigned the specified value, and any subsequent references to the variable will work as expected.
