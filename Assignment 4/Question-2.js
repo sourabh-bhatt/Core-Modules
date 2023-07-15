5fb77933a37144d5422ec39ef52e0e60259ec45f
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-15 15:31:16 +0530

    updated ans 2

--- Assignment 4/Question-2.js
+++ Assignment 4/Question-2.js
@@ -11,3 +11,4 @@ console.log(myVariable);
 
 // Above, the first console.log statement throws a ReferenceError because myVariable is accessed before it is declared. This is because the variable is in the temporal dead zone until it reaches the line where it is declared.
 // Once the variable declaration is encountered, it is assigned the specified value, and any subsequent references to the variable will work as expected.
+// We can easily hoist the let and const but they cant be accessed before the initialization
