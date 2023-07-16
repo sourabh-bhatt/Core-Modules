aafe58e855f8cfdaf8c7879674873141429d8835
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-16 23:57:30 +0530

    Q3 completed

---
+++ Assignment 5/Q3.js
@@ -0,0 +1,8 @@
+// setTimeout() schedules a function to be executed once after a specified number of milliseconds.
+
+// setInterval() schedules a function to be executed repeatedly at a specified interval of milliseconds.
+
+// They can be executed as:
+
+// setTimeout(function, milliseconds);
+// setInterval(function, milliseconds);
