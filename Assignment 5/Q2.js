6596af87bccd6948df48c0dd7f7ebfc624f32f7d
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-16 23:55:52 +0530

    Q2 completed

---
+++ Assignment 5/Q2.js
@@ -0,0 +1,7 @@
+// A web API is an application programming interface (API) that allows communication between two software applications over the web. Web APIs are typically used to access data or functionality from a remote server.
+
+// Web APIs are often used to build web applications that can interact with other web services. For example, a web application that allows users to search for products might use a web API to access product data from a remote server.
+
+// Web APIs are also used to build mobile applications. For example, a mobile app that allows users to check the weather might use a web API to access weather data from a remote server.
+
+// Web APIs are typically implemented using HTTP. HTTP is a standard protocol for exchanging data over the web. HTTP requests and responses are typically encoded in JSON or XML.
