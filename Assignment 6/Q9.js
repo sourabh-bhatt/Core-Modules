5d6c84611d45e064d05b61248302450e5637f5b9
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-17 16:51:31 +0530

    Q8-9 completed

---
+++ Assignment 6/Q9.js
@@ -0,0 +1,12 @@
+// A class in JavaScript is a blueprint for creating objects. It defines the properties and methods that an object will have.
+
+// Classes are defined using the class keyword.
+
+// For example, the following code defines a class called Animal:
+
+class Animal {
+  name = "Animal";
+  speak() {
+    return "I am an animal";
+  }
+}
