de71b9da85729c37753fc41f8a609b516afc505b
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-17 09:42:41 +0530

    Q1 Completed

---
+++ Assignment 6/Q1.js
@@ -0,0 +1,15 @@
+// A constructor is a special type of function that is used to create new objects.
+// It is called when an object is created using the new keyword.
+
+// The constructor is responsible for initializing the object's properties and methods.
+// It can also be used to perform any other necessary initialization tasks.
+
+// For example
+
+function person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+const student = new person("Sourabh", 20);
+console.log(student.name);
+console.log(student.age);
