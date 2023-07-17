f883cac7c8f931570336610ebe3fa4b7b8a8d8c6
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-17 09:25:17 +0530

    Q7 completed

---
+++ Assignment 5/Q7.js
@@ -0,0 +1,11 @@
+// Async and await are keywords in JavaScript allows us to write asynchronous code in a more concise and readable way.
+// Async functions are functions that return a promise, and the await keyword is used to wait for the promise to be fulfilled before continuing execution.
+
+// For example
+
+async function doSomethingAsync() {
+  const result = await someAsyncOperation();
+  console.log(result);
+}
+
+doSomethingAsync();
